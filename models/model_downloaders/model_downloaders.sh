591cf37413a576f1b669d131c1b27e7153db7fa4
Name <test@example.com>
2023-04-24 10:13:18 +0800

    Chaanging models to downlaod.

--- models/model_downloaders/model_downloaders.sh
+++ models/model_downloaders/model_downloaders.sh
@@ -1,9 +1,20 @@
 # you may specify model families and checkpoints here
-model_family="FLAN-T5"
-checkpoint="google/flan-t5-small"
+# model_family="FLAN-T5"
+model_families=("GPT2" "T5")
+# checkpoint="google/flan-t5-small"
+checkpoints=("google/flan-t5-small" "google/flan-t5-base" "google/flan-t5-large" "google/flan-t5-xl")
 
+# for checkpoint in "${checkpoints[@]}"
+# do
+#     python models/model_downloaders/model_downloaders.py \
+#         --model_family ${model_family} \
+#         --checkpoint ${checkpoint} \
+#         # --download_all_checkpoints 
+# done
 
-python models/model_downloaders/model_downloaders.py \
-    --model_family ${model_family} \
-    --checkpoint ${checkpoint} \
-    --download_all_checkpoints \
+for model_family in "${model_families[@]}"
+do
+    python models/model_downloaders/model_downloaders.py \
+        --model_family ${model_family} \
+        --download_all_checkpoints
+done
