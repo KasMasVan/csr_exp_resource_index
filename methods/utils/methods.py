2f606d0c42cf117934c99b951b9d5e92aff172f4
Kevin <51369075+KasMasVan@users.noreply.github.com>
2023-04-25 17:36:15 +0800

    Working on GPT reasoning. Something goes wrong...

--- methods/utils/methods.py
+++ methods/utils/methods.py
@@ -123,21 +123,19 @@ def compute_causal_conditional_score(batch, model, device):
     # adding this line of code takes me more than an hour.
     # without adding torch.no_grad, GPU usage will muiltply by 4.
     with torch.no_grad():
-        # outputs = model(input_ids = header_input_ids, 
-        #                 attention_mask = header_attention_mask,
-        #                 labels = ending_input_ids)
         outputs = model(input_ids = input_ids,
                         attention_mask = attention_mask,
                         labels = labels)
     
     _, logits = outputs.loss, outputs.logits
+    # shift
+    logits = logits[:, :-1].contiguous()
+    labels = labels[:, 1:].contiguous()
     # e.g., (batch_size * #option, ending_seq_len, #vocab): (64, 18, 32128)
     logits = logits.view(-1, logits.shape[-1])
     # ignore padding token: 50256
-    # ce_loss = F.cross_entropy(logits, ending_input_ids.view(-1), reduction="none", ignore_index=0).detach().cpu()
     ce_loss = F.cross_entropy(logits, labels.view(-1), reduction="none", ignore_index=padding_token).detach().cpu()
     # each score is the negative log-likelihood of a ending given a header.
     # batch_predictions = ce_loss.view(ending_shape).sum(dim=-1).argmin(dim=-1)
-    # log_prob = ce_loss.view(labels.shape).sum(dim=-1)
     log_prob = ce_loss.view(batch["ending_input_ids"].shape[0], batch["ending_input_ids"].shape[1], -1).sum(dim=-1)
     return log_prob
