3c505e3146d1380b22479cac2ed207ff92f23257
Kevin <51369075+KasMasVan@users.noreply.github.com>
2023-05-10 11:20:52 +0800

    Plot performance on each dataset.

--- methods/my_exp.sh
+++ methods/my_exp.sh
@@ -1,11 +1,11 @@
 seed=0
 model_family="FLAN-T5"
-# checkpoints=("EleutherAI/pythia-160m-deduped" "EleutherAI/pythia-410m-deduped" "EleutherAI/pythia-1b-deduped")
-checkpoints=("google/flan-t5-small")
+# checkpoints=("EleutherAI/pythia-70m-deduped" "EleutherAI/pythia-160m-deduped" "EleutherAI/pythia-410m-deduped")
+checkpoints=("google/flan-t5-small" "google/flan-t5-base" "google/flan-t5-large")
 # amateur_checkpoint="google/flan-t5-small"
 # expert_checkpoint="google/flan-t5-base"
 datasets="cqa copa obqa piqa qasc siqa winogrande"
-batch_size=32 
+batch_size=16
 
 multiple_choice_prompt="Question:"
 
@@ -27,18 +27,18 @@ do
     #     --batch_size  ${batch_size} \
 
     # multiple choice prompt, using the same script as language modeling
-    # python language_modeling.py \
-    #     --model_family ${model_family} \
-    #     --checkpoint ${checkpoint} \
-    #     --datasets "$datasets" \
-    #     --batch_size  ${batch_size} \
-    #     --multiple_choice_prompt ${multiple_choice_prompt}
+    python language_modeling.py \
+        --model_family ${model_family} \
+        --checkpoint ${checkpoint} \
+        --datasets "$datasets" \
+        --batch_size  ${batch_size} \
+        --multiple_choice_prompt ${multiple_choice_prompt}
 
     # process of elimination
-    # python process_of_elimination.py \
-    #     --model_family ${model_family} \
-    #     --checkpoint ${checkpoint} \
-    #     --datasets "$datasets" \
-    #     --batch_size  ${batch_size} \
-    #     --multiple_choice_prompt ${multiple_choice_prompt}
+    python process_of_elimination.py \
+        --model_family ${model_family} \
+        --checkpoint ${checkpoint} \
+        --datasets "$datasets" \
+        --batch_size  ${batch_size} \
+        --multiple_choice_prompt ${multiple_choice_prompt}
 done
