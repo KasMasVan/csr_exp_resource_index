240a1bdc762697bc8e03b572272cc1a7ff9665a0
Kevin <51369075+KasMasVan@users.noreply.github.com>
2023-05-15 18:50:39 +0800

    Small update

--- methods/my_exp.sh
+++ methods/my_exp.sh
@@ -5,8 +5,8 @@ loading_precision="FP32"
 # checkpoints=("google/flan-t5-small" "google/flan-t5-base" "google/flan-t5-large")
 # amateur_checkpoint="google/flan-t5-small"
 # expert_checkpoint="google/flan-t5-base"
-# datasets="cqa copa obqa piqa qasc siqa winogrande"
-datasets="cqa"
+datasets="cqa copa obqa piqa qasc siqa winogrande"
+# datasets="cqa"
 batch_size=16
 sample=100
 
@@ -33,24 +33,24 @@ do
     #     --batch_size  ${batch_size} \
 
     # multiple choice prompt, using the same script as language modeling
-    # python language_modeling.py \
-    #     --model_family ${model_family} \
-    #     --checkpoint ${checkpoint} \
-    #     --datasets "$datasets" \
-    #     --batch_size  ${batch_size} \
-    #     --loading_precision ${loading_precision} \
-    #     --multiple_choice_prompt ${multiple_choice_prompt} \
-    #     --push_data_to_hub \
-    #     --sample ${sample} \
+    python language_modeling.py \
+        --model_family ${model_family} \
+        --checkpoint ${checkpoint} \
+        --datasets "$datasets" \
+        --batch_size  ${batch_size} \
+        --loading_precision ${loading_precision} \
+        --multiple_choice_prompt ${multiple_choice_prompt} \
+        # --push_data_to_hub \
+        # --sample ${sample} \
 
     # process of elimination
-    # python process_of_elimination.py \
-        # --model_family ${model_family} \
-        # --checkpoint ${checkpoint} \
-        # --loading_precision ${loading_precision} \
-        # --datasets "$datasets" \
-        # --batch_size  ${batch_size} \
-        # --multiple_choice_prompt ${multiple_choice_prompt} \
+    python process_of_elimination.py \
+        --model_family ${model_family} \
+        --checkpoint ${checkpoint} \
+        --loading_precision ${loading_precision} \
+        --datasets "$datasets" \
+        --batch_size  ${batch_size} \
+        --multiple_choice_prompt ${multiple_choice_prompt} \
         # --push_data_to_hub 
         # --sample ${sample} 
 done
